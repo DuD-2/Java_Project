d58dc0cacbdc2fd9d3a4cdc9920a44dc1cbef5b5
Likcc <ryabovdaniil97@gmail.com>
2024-06-25 20:13:16 +0700

    Update ExchangeRateRepository.java

--- ExchangeRates/src/main/java/com/example/demo/Repositories/ExchangeRateRepository.java
+++ ExchangeRates/src/main/java/com/example/demo/Repositories/ExchangeRateRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDate;
 import java.util.List;
 
-public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, Long> {
+public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, Long> { //не увидел имплементации класса
     List<ExchangeRate> findByCharCodeAndDate(String code, LocalDate date);
 }
